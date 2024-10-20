21f646a2843b37c30e95628f237c1a95cf8a2c91
10530 <harshal.giri@neml.in>
2024-10-20 21:07:40 +0530

    projects

--- quartz_schedular/quartz_schedular_1/src/main/java/Schedular.java
+++ quartz_schedular/quartz_schedular_1/src/main/java/Schedular.java
@@ -1,7 +1,11 @@
 
 import org.quartz.*;
+import org.quartz.impl.calendar.HolidayCalendar;
 
+import java.time.LocalDate;
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
 
 public class Schedular {
     public static void main(String[] args) throws SchedulerException {
@@ -34,5 +38,40 @@ public class Schedular {
 
         // Tell quartz to schedule the job using our trigger
         scheduler.scheduleJob(job, trigger);
+
+
+        HolidayCalendar cal = new HolidayCalendar();
+        Date date = new Date();
+
+        Calendar day1 = Calendar.getInstance();
+        day1.setTime(date);
+        day1.add(Calendar.DATE, 1);
+
+        Calendar day2 = Calendar.getInstance();
+        day2.setTime(date);
+        day2.add(Calendar.DATE, 2);
+
+        cal.addExcludedDate( day1.getTime() );
+        cal.addExcludedDate( day2.getTime() );
+
+        scheduler.addCalendar("HolidayCalendar", cal , false, false);
+
+        Trigger trigger1 = TriggerBuilder
+                .newTrigger()
+                .forJob(job)
+                .withIdentity("holiday-trigger-1")
+                .modifiedByCalendar("HolidayCalendar")
+                .withSchedule(DailyTimeIntervalScheduleBuilder.dailyTimeIntervalSchedule().onEveryDay())
+                .build();
+
+        Trigger trigger2 = TriggerBuilder
+                .newTrigger()
+                .forJob(job)
+                .withIdentity("holiday-trigger-2")
+                .modifiedByCalendar("HolidayCalendar")
+                .withSchedule(DailyTimeIntervalScheduleBuilder.dailyTimeIntervalSchedule().onMondayThroughFriday())
+                .build();
+
+        scheduler.scheduleJob(job, trigger2);
     }
 }
